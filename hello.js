4db289c736bd46ebd47c04a99fd0c5c0f2f81007
Harjot Sandhu <harjot418@gmail.com>
2020-05-04 17:31:25 +0000

    Name print function

---
+++ hello.js
@@ -0,0 +1,12 @@
+const sayHello1  = function () {
+  console.log("Hello, world");
+}
+
+sayHello1();
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
