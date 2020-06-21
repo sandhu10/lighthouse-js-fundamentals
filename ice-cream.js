2ccd31add780080622c76765724258b5541bdae2
Harjot Sandhu <harjot418@gmail.com>
2020-06-21 22:00:39 +0000

    Ice Cream flavour array intro

---
+++ ice-cream.js
@@ -0,0 +1,18 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+//add to array end
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//print first flavour
+console.log(iceCreamFlavours[0]);
+
+//print last flavour
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+//print total number of flavours
+console.log(iceCreamFlavours.length - 1);
+
